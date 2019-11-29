507f5a076a064f0698ccc74895b55907bcd0c613
Samridh Harshit <samridhharsh@gmail.com>
2019-11-29 22:33:18 +0530

    final update

--- client/src/components/customerOptions.jsx
+++ client/src/components/customerOptions.jsx
@@ -1,6 +1,7 @@
 import  React from "react";
 import {Button} from "reactstrap";
 
+// importing forms for checkin and checkout
 import CustomerCheckInForm from "./CustomerCheckInForm";
 import CustomerCheckOutForm from "./CustomerCheckOutForm";
 
@@ -12,11 +13,13 @@ class CustomerOptions extends React.Component {
         checkOutDisplay: false
     };
 
+    //sets state of "showCustomerCheckInForm" to true
     handleCheckIn = () => {
         this.setState({showCustomerCheckInForm: true,
             checkOutDisplay: true});
     };
 
+    //sets state of "showCustomerCheckOutForm" to true
     handleCheckOut = () => {
         this.setState({showCustomerCheckOutForm: true,
             checkInDisplay: true})
@@ -29,6 +32,7 @@ class CustomerOptions extends React.Component {
             <div className="customerOptions">
                 <div className={this.state.checkInDisplay ? 'checkin displaynone' : 'checkin'}>
                     {
+                        //if the state is true, redirects to the checkin component else shows a button to set it true
                         this.state.showCustomerCheckInForm ?
                             <CustomerCheckInForm /> :
                             <Button onClick={this.handleCheckIn}>CheckIn</Button>
@@ -36,6 +40,7 @@ class CustomerOptions extends React.Component {
                 </div>
                 <div className={this.state.checkOutDisplay ? 'checkout displaynone' : 'checkout'}>
                     {
+                        //if the state is true, redirects to the checkin component else shows a button to set it true
                         this.state.showCustomerCheckOutForm ?
                             <CustomerCheckOutForm /> :
                             <Button onClick={this.handleCheckOut}>CheckOut</Button>
